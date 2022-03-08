798a72d3ab4a84ee6ba4ce3187b29c60cfd25bb6
iLynette <lynetteacholah@icloud.com>
2022-03-08 07:09:33 +0300

    Find an API

---
+++ src/modules/utils.js
@@ -0,0 +1,5 @@
+const myId = 'u_id';
+const involvementApi = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${myId}`
+const aniApi = 'https://api.aniapi.com';
+
+export { involvementApi, aniApi };
