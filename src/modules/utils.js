a4f2e8427d05e8f4f50031b3b27081b4d523b023
Cynthia Inga <candyinga67@gmail.com>
2022-03-08 10:05:04 +0200

    Add an unique Id

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,5 +1,4 @@
-const myId = 'u_id';
-const involvementApi = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${myId}`;
+const involvementApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/UA1qh5oDK24p7rhA1W5m';
 const aniApi = 'https://api.aniapi.com';
 
 export { involvementApi, aniApi };
