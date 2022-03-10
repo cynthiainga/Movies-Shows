211a0ca267b487497d0a15c20864add82e056dee
iLynette <lynetteacholah@icloud.com>
2022-03-10 11:27:51 +0300

    Update likes

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -16,3 +16,13 @@ export const postLikes = async (id) => {
     })
     return res;
 };
+
+export const updateLikes = async (id, display) => {
+    const res = await fetch(involvementApi);
+    const saveLikes = await res.json();
+    saveLikes.forEach((show) => {
+        if (show.item_id === id) {
+            display.value = `${show.likes}`
+        }
+    });
+};
