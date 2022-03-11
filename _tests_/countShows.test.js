b73dbfc098a44267750c96fd11c8f89d8f50543f
Cynthia Inga <candyinga67@gmail.com>
2022-03-11 19:48:44 +0200

    Add more style

--- _tests_/countShows.test.js
+++ _tests_/countShows.test.js
@@ -1,15 +1,15 @@
-import countShow from "../src/modules/counts";
+import countShow from '../src/modules/counts.js';
 
 describe('test the count show function', () => {
-    test('check if the shows are equal to 5', () => {
-        const movies = [
-            {name: 'Dragon ball'}, 
-            {name: 'Code geass'}, 
-            {name: 'Naruto'}, 
-            {name: 'Bleach'}, 
-            {name: 'AOT'}];
-        const count = countShow(movies)
+  test('check if the shows are equal to 5', () => {
+    const movies = [
+      { name: 'Dragon ball' },
+      { name: 'Code geass' },
+      { name: 'Naruto' },
+      { name: 'Bleach' },
+      { name: 'AOT' }];
+    const count = countShow(movies);
 
-        expect(count).toBe(5)
-    })
-})
+    expect(count).toBe(5);
+  });
+});
