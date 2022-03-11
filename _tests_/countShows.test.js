aeffb5b4cc03c6e48b74be9ece7d783f44935c1d
iLynette <lynetteacholah@icloud.com>
2022-03-11 19:29:21 +0300

    Add test for counting shows

---
+++ _tests_/countShows.test.js
@@ -0,0 +1,15 @@
+import countShow from "../src/modules/counts";
+
+describe('test the count show function', () => {
+    test('check if there is data equal to 5', () => {
+        const movies = [
+            {name: 'Dragon ball'}, 
+            {name: 'Code geass'}, 
+            {name: 'Naruto'}, 
+            {name: 'Bleach'}, 
+            {name: 'AOT'}];
+        const count = countShow(movies)
+
+        expect(count).toBe(5)
+    })
+})
